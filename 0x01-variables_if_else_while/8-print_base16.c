b33909715c67d9f0e15804498af93665ade83a80
olu-0729 <olusykes07@gmail.com>
2022-09-09 20:10:24 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,4 @@
-[3:43 AM, 9/10/2022] B.T: #include <stdlib.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
@@ -29,34 +29,3 @@ int main(void)
 
 	return (0);
 }
-[3:56 AM, 9/10/2022] B.T: #include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-
-/**
- * main - prints all possible combinations of single-digit numbers
- *
- * Return: 0
- */
-int main(void)
-{
-	int i;
-
-	i = 48;
-	while  (i < 58)
-	{
-		putchar(i);
-		if (i !=  57)
-		{
-			putchar(44);
-			putchar(32);
-			i++;
-		}
-		else
-		{
-			i++;
-		}
-	}
-	putchar('\n');
-
-	return (0);
