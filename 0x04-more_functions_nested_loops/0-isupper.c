13dbf3c72c66ce100c59194e334de9f8f77ade15
olu-0729 <olusykes07@gmail.com>
2022-09-15 12:51:18 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to print
+ *
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	if ((c >= 65) && (c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+
+    Terms
+    Privacy
+    Security
+    Status
+
