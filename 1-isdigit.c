2ca6e146d2f1eb8476617eaa8be4ed094b071f59
olu-0729 <olusykes07@gmail.com>
2022-09-15 13:28:53 -0700

    1-isdigit.c

---
+++ 1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit
+ * @c: the parameter to print
+ *
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+Footer
+© 2
