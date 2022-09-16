3ddf3db19b0ce75c002958f30b41283f14fae412
olu-0729 <olusykes07@gmail.com>
2022-09-16 02:29:23 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers 0 to 9
+ *
+ * Return: numbers
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 48 ; num <= 57 ; num++)
+		_putchar(num);
+	_putchar('\n');
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+
+
