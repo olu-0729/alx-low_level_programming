53987812abbb9073b536a0b08908ff40365bf5e2
olu-0729 <olusykes07@gmail.com>
2022-09-19 13:03:09 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *  void prototypes()
+ *  int prototypes()
+ */
+ 
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *3);
+void _puts(char *str);
+void prints_rev(char *3);
+void rev_string(char *3);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *str);
+
+#endif /*MAIN_H*/
