54a1d2203f61698fae195d0bf939bb4ec2141b1d
olu-0729 <olusykes07@gmail.com>
2022-09-26 13:38:01 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,9 @@
 /**
- * _strspn - Gets the length of a prefix substring.
- * @s: string where substring will look
- * @accept: substring of accepted chars
- * Return: length of occurrence
- */
+* _strspn - Gets the length of a prefix substring.
+* @s: String where substring will look.
+* @accept: Substring of accepted chars.
+* Return: Length of occurrence.
+*/
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int c = 0;
@@ -12,14 +12,14 @@ unsigned int _strspn(char *s, char *accept)
 	while (*s++)
 	{
 		while (*accept++)
-			if (*(s - 1)  == *(accept - 1))
-                        {
+			if (*(s - 1) == *(accept - 1))
+			{
 				c++;
 				break;
 			}
 		if (!(*--accept))
 			break;
 		accept = t;
-		 }
-	return (c)
+	}
+	return (c);
 }
