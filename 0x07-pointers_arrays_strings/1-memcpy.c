c7f01de34342ade3f05ec59c270772e62801c8e5
olu-0729 <olusykes07@gmail.com>
2022-09-26 12:27:43 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - irontnoux2
+ * @dest: pointer to char param
+ * @src: pointer to char param
+ * @n: size
+ * Return: *dest
+ */
+
+char *_memcpy(*dest, *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = n; i < n; i++)
+		dest[i] = src[i];
+	}
+        return (dest);
+}
