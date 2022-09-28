4d469ca0f9a5608cf9db317d99b4b49cc6de1d4b
olu-0729 <olusykes07@gmail.com>
2022-09-28 04:11:56 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,4 +1,4 @@
-#include "main.h"
+ #include "main.h"
 
 /**
  * _pow_recursion - pow recursion
@@ -9,8 +9,8 @@
 
 
 int _pow_recursion(int x, int y)
-{ 
-	if (y < 0) 
+{
+	if (y < 0)
 	{
 		return (-1);
 	}
@@ -18,5 +18,5 @@ int _pow_recursion(int x, int y)
 	{
 		return (1);
 	}
-	return (x * _pow_recursion(x, y -1));
+	return (x * _pow_recursion(x, y - 1));
 }
